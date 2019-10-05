481a6f83d35a531752b7243c87ec00342e807f7f
FabioJSilva <fabiopt6@gmail.com>
2019-10-05 20:26:00 -0300

    Configuraçao das Rotas

---
+++ src/pages/Login/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+export default function Login() {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email, password });
+    const { _id } = response.data;
+
+    localStorage.setItem('user_id', _id);
+  }
+
+  return (
+    <>
+      <p>
+        Ofereça
+        <strong> spots </strong>
+        para programadores e encontre
+        <strong> talentos </strong>
+        para sua empresa!
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-MAIL *</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email cadastrado"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+
+        <label htmlFor="password">SENHA *</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Senha cadastrada"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+
+        <button className="btn" type="submit">Entrar</button>
+      </form>
+    </>
+  );
+}
