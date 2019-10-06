93ad35e91ef9f05210c253ff61049810d9acbf9d
FabioJSilva <fabiopt6@gmail.com>
2019-10-06 13:39:56 -0300

    Criaçao da page de Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+import './style.css';
 
 export default function Dashboard() {
-  return <div />;
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    async function loadSpots() {
+      const userId = localStorage.getItem('user_id');
+      console.log(userId);
+      const response = await api.get('/dashboard', {
+        headers: { user_id: userId }
+      });
+      console.log(response.data);
+
+      setSpots(response.data);
+    }
+
+    loadSpots();
+  }, []);
+  return (
+    <>
+      <ul className="spot-list">
+        {spots.map((spot) => (
+          <li key={spot._id}>
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
+            <strong>{spot.company}</strong>
+            <span>{spot.price ? `R$ ${spot.price}` : 'Gratuito!'}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
